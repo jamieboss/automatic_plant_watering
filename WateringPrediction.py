914a724381d83317e2dda201f1e3ac4f39a16c7f
Aditya Vadlamani <vadlamani.12@osu.edu>
2023-04-30 21:57:01 -0400

    Add axes labels and legend

--- WateringPrediction.py
+++ WateringPrediction.py
@@ -14,8 +14,7 @@ THRESHOLD = [0.55, 0.4, 0.35, 0.3, 0.25, 0.35]
 PLANT_DATA = PLANT_DATA.replace({ water_levels[i] : THRESHOLD[i] for i in range(len(water_levels))} | { "Cacti & Succulents": 0, "Flowering Plants": 1, "Foliage Plants": 2, "Herbs": 3})
 
 PLANT_DATA = PLANT_DATA[["Plant Type", "Water Level"]]
-PLANT_DATA["Last Watering"] = np.random.randint(1, 7, PLANT_DATA.shape[0]) * np.random.rand(PLANT_DATA.shape[0]) * 86400000
-print(PLANT_DATA.values)
+PLANT_DATA["Last Watering"] = np.random.randint(1, 7, PLANT_DATA.shape[0]) * np.random.rand(PLANT_DATA.shape[0])
 
 kmeans = KMeans(2, random_state=0, n_init="auto").fit(PLANT_DATA.values)
 labels = kmeans.labels_
@@ -26,7 +25,11 @@ label1 = PLANT_DATA[PLANT_DATA["Labels"] == 1]
 label0 = PLANT_DATA[PLANT_DATA["Labels"] == 0]
 
 ax = plt.axes(projection='3d')
-ax.scatter(label1["Plant Type"], label1["Water Level"], label1["Last Watering"], c="red")
-ax.scatter(label0["Plant Type"], label0["Water Level"], label0["Last Watering"], c="blue")
 
+ax.scatter(label1["Plant Type"], label1["Water Level"], label1["Last Watering"], c="blue", label="Needs water")
+ax.scatter(label0["Plant Type"], label0["Water Level"], label0["Last Watering"], c="red", label = "Has enough water")
+ax.set_xlabel("Plant Type")
+ax.set_ylabel("Desired Moisture Level")
+ax.set_zlabel("Last Watering (Days)")
+ax.legend()
 plt.show()
